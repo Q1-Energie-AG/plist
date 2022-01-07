e8370b9fdd193e961341d2266c061c971e510a46
Kevin Seidel <k.seidel@q1.eu>
2022-01-07 13:35:31 +0100

    Add data to binary tests

--- test/plist_test.exs
+++ test/plist_test.exs
@@ -10,6 +10,7 @@ defmodule PlistTest do
     assert Map.get(plist, "Array") == ["A", "B", "C"]
     assert Map.get(plist, "Date") == "2015-11-17 14:00:59 +0000"
     assert Map.get(plist, "True") == true
+    assert Map.get(plist, "Base64") == <<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>
     assert Map.get(plist, "SomeUID")["CF$UID"] == 40
   end
 
